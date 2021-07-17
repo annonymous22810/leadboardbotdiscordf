7e6df06378f65ec5309eb15dbe8591952591470e
annonymous22810 <87542753+annonymous22810@users.noreply.github.com>
2021-07-17 10:24:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -692,7 +692,6 @@ console.log(`Command: ${server.toLowerCase()} used by: `+ message.author.usernam
 					})
 }
 
-var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.send('it is running\n'); 
